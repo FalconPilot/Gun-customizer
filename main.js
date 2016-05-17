c1a3be781298eb26f742e74076f3dcb6bca50b14
FalconPilot <fpilot.github@gmail.com>
2016-05-17 03:15:46 +0200

    updated main script

--- main.js
+++ main.js
@@ -1,20 +1,48 @@
 const electron = require('electron')
+const fs = require('fs')
+const path = require('path')
 const app = electron.app
 const BrowserWindow = electron.BrowserWindow
 
 let mainWindow
 
+// Creating window
+
 function createWindow() {
-  mainWindow = new BrowserWindow({
+  win = new BrowserWindow({
     fullscreen: true,
-    title: 'Gun customizer'
+    title: 'Gun customizer',
+    webPreferences: {
+
+    }
   })
-  mainWindow.loadURL('file://' + __dirname + '/index.html')
-  mainWindow.webContents.openDevTools()
-  mainWindow.on('closed', function() {
+  win.loadURL('file://' + __dirname + '/index.html')
+  win.webContents.openDevTools()
+  win.on('closed', function() {
     app.quit()
   })
+  loadParts()
 }
+
+// Parts loading
+
+function loadParts() {
+  gunsdir = __dirname + '/resources/guns'
+  guns = getDirectories(gunsdir)
+  for (i = 0; i < guns.length; i++) {
+    console.log(guns[i])
+  }
+}
+
+// Get directories
+
+function getDirectories(src) {
+  return fs.readdirSync(src).filter(function(file) {
+    return fs.statSync(path.join(src, file)).isDirectory()
+  })
+}
+
+// App event listeners
 
 app.on('ready', function() {
   createWindow()
