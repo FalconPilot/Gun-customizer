3b93a85bb8671d963bda613ba18d04644697a88f
FalconPilot <fpilot.github@gmail.com>
2016-05-17 06:35:23 +0200

    improved rendering method and supported weapon change

--- scripts/rendering.js
+++ scripts/rendering.js
@@ -1,8 +1,9 @@
 const remote = require('electron').remote
+const data = remote.getCurrentWindow().rendererSideName
 const frame = document.getElementById('frame')
 const loading = document.getElementById('loading')
 const gunlist = document.getElementById('gunlist')
-const data = remote.getCurrentWindow().rendererSideName
+const button = document.getElementById('change_gun')
 
 // Handle content loading
 
@@ -14,25 +15,37 @@ function loadContent() {
     option.innerHTML = weapons[i].name.toUpperCase()
     gunlist.appendChild(option)
   }
-  defaultWeapon(data)
-  switchLoading()
+  button.addEventListener("click", function() {
+    switchLoading()
+    setTimeout(function() {
+      switchWeapon(document.getElementById('gunlist').value)
+    }, 600)
+  })
+  switchWeapon(0)
 }
 
 
 // Load default weapon
 
-function defaultWeapon(data) {
-  weapon = data['weapons'][0]
+function switchWeapon(index) {
+  frame.innerHTML = ""
+  weapon = data['weapons'][index]
   for (i = 0; i < weapon.nodelist.length; i++) {
     part = weapon.nodelist[i].partslist[0]
     img = document.createElement('img')
     img.src = part.src
     img.style.zIndex = weapon.nodelist[i].zlevel
     img.onload = function() {
-      frame.style.width = this.width + "px"
+      if (frame.style.width === '') {
+        frame.style.width = this.width + "px"
+        frame.style.height = this.height + "px"
+      }
       frame.appendChild(this)
     }
   }
+  setTimeout(function() {
+    switchLoading()
+  }, 200)
 }
 
 // Switch loading status
@@ -42,19 +55,23 @@ function switchLoading() {
   if (loading.style.zIndex != '') {
     loading.style.zIndex = null
     loading.style.opacity = null
-    for (i = 0; i < nodes.length; i++) {
-      if (nodes[i].nodeType != 3) {
-        nodes[i].style.animation = null
+    setTimeout(function() {
+      for (i = 0; i < nodes.length; i++) {
+        if (nodes[i].nodeType != 3) {
+          nodes[i].style.animation = null
+        }
       }
-    }
+    }, 600)
   } else {
     loading.style.opacity = '0.0'
-    loading.style.zIndex = '-1'
-    for (i = 0; i < nodes.length; i++) {
-      if (nodes[i].nodeType != 3) {
-        nodes[i].style.animation = 'none'
+    setTimeout(function() {
+      loading.style.zIndex = '-1'
+      for (i = 0; i < nodes.length; i++) {
+        if (nodes[i].nodeType != 3) {
+          nodes[i].style.animation = 'none'
+        }
       }
-    }
+    }, 600)
   }
 }
 
